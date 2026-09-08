9401d66a020f0c0cce0671d0d0658b2fecb3f0bd
Gtkd <guilhermejesusnascimento255@gmail.com>
2026-09-08 22:08:04 +0000

    penisenormes

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sistema Escola",
+    page_icon="🏫",
+    layout="wide"
+)
+
+st.title("🏫 Sistema Escola")
+st.success("Streamlit conectado com sucesso!")
+
+st.write("Painel administrativo do Sistema Escola.")
