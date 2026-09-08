443bebf492347ed41336a6c463e0883aa29408e5
Gtkd <guilhermejesusnascimento255@gmail.com>
2026-09-08 22:54:10 +0000

    doad

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Use somente a chave pública "anon". Nunca coloque a service_role neste arquivo.
 window.ESCOLA_CONFIG = {
-  supabaseUrl: "https://uiycrfjevzozlmzudfyd.supabase.co/rest/v1/",
-  supabaseAnonKey: "sb_publishable_9NQMNMIK2eAFDK0A-NVk9Q_Zd3VCa3_"
+  supabaseUrl: "https://uiycrfjevzozlmzudfyd.supabase.co",
+  supabaseAnonKey: "sb_publishable_9NQMNMlK2eAFDK0A-NVk9Q_Zd3VCa3_"
 };
 
